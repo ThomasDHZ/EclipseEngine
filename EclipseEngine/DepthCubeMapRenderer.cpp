e9651170e6679f364826dd09951939d4ecd1d7c8
asdf <dothackzero@outlook.com>
2022-08-17 11:31:53 -0400

    z

--- EclipseEngine/DepthCubeMapRenderer.cpp
+++ EclipseEngine/DepthCubeMapRenderer.cpp
@@ -1,1 +1,179 @@
 #include "DepthCubeMapRenderer.h"
+#include "LightManager.h"
+
+
+DepthCubeMapRenderer::DepthCubeMapRenderer() : RenderPass()
+{
+}
+
+DepthCubeMapRenderer::~DepthCubeMapRenderer()
+{
+}
+
+void DepthCubeMapRenderer::BuildRenderPass(glm::vec2 TextureResolution)
+{
+    SampleCount = VK_SAMPLE_COUNT_1_BIT;
+    RenderPassResolution = TextureResolution;
+
+    if (renderPass == nullptr)
+    {
+        DepthTexture = std::make_shared<RenderedCubeMapDepthTexture>(RenderedCubeMapDepthTexture(RenderPassResolution, SampleCount));
+    }
+    else
+    {
+        DepthTexture->RecreateRendererTexture(RenderPassResolution);
+        RenderPass::Destroy();
+    }
+
+    std::vector<VkImageView> AttachmentList;
+    AttachmentList.emplace_back(DepthTexture->View);
+
+    RenderPassDesc();
+    CreateRendererFramebuffers(AttachmentList);
+    BuildRenderPassPipelines();
+    SetUpCommandBuffers();
+}
+
+void DepthCubeMapRenderer::RenderPassDesc()
+{
+    std::vector<VkAttachmentDescription> AttachmentDescriptionList;
+    AttachmentDescriptionList.emplace_back(DepthTexture->GetAttachmentDescription());
+
+
+    VkAttachmentReference depthReference = { 0, VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL };
+
+    VkSubpassDescription subpassDescription = {};
+    subpassDescription.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpassDescription.pDepthStencilAttachment = &depthReference;
+
+    std::vector<VkSubpassDependency> DependencyList;
+
+    VkSubpassDependency FirstDependency = {};
+    FirstDependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+    FirstDependency.dstSubpass = 0;
+    FirstDependency.srcStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+    FirstDependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+    FirstDependency.srcAccessMask = VK_ACCESS_SHADER_READ_BIT;
+    FirstDependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+    FirstDependency.dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+    DependencyList.emplace_back(FirstDependency);
+
+    VkSubpassDependency SecondDependency = {};
+    SecondDependency.srcSubpass = 0;
+    SecondDependency.dstSubpass = VK_SUBPASS_EXTERNAL;
+    SecondDependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+    SecondDependency.dstStageMask = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+    SecondDependency.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+    SecondDependency.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+    SecondDependency.dependencyFlags = VK_DEPENDENCY_BY_REGION_BIT;
+    DependencyList.emplace_back(SecondDependency);
+
+    VkRenderPassCreateInfo renderPassInfo = {};
+    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    renderPassInfo.attachmentCount = static_cast<uint32_t>(AttachmentDescriptionList.size());
+    renderPassInfo.pAttachments = AttachmentDescriptionList.data();
+    renderPassInfo.subpassCount = 1;
+    renderPassInfo.pSubpasses = &subpassDescription;
+    renderPassInfo.dependencyCount = static_cast<uint32_t>(DependencyList.size());
+    renderPassInfo.pDependencies = DependencyList.data();
+
+    if (vkCreateRenderPass(VulkanRenderer::GetDevice(), &renderPassInfo, nullptr, &renderPass))
+    {
+        throw std::runtime_error("Failed to create Buffer RenderPass.");
+    }
+
+}
+
+void DepthCubeMapRenderer::BuildRenderPassPipelines()
+{
+    VkPipelineColorBlendAttachmentState ColorAttachment;
+    ColorAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+    ColorAttachment.blendEnable = VK_FALSE;
+    ColorAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
+    ColorAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+    ColorAttachment.colorBlendOp = VK_BLEND_OP_ADD;
+    ColorAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+    ColorAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+    ColorAttachment.alphaBlendOp = VK_BLEND_OP_SUBTRACT;
+
+    ColorAttachmentList.clear();
+    ColorAttachmentList.resize(2, ColorAttachment);
+
+    PipelineInfoStruct pipelineInfo{};
+    pipelineInfo.renderPass = renderPass;
+    pipelineInfo.ColorAttachments = ColorAttachmentList;
+    pipelineInfo.SampleCount = SampleCount;
+
+    depthCubeMapPipeline.InitializePipeline(pipelineInfo);
+}
+
+VkCommandBuffer DepthCubeMapRenderer::Draw(glm::vec3 CubeSamplerPos)
+{
+
+    VkCommandBufferBeginInfo beginInfo{};
+    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+    std::array<VkClearValue, 1> clearValues{};
+    clearValues[0].depthStencil = { 1.0f, 0 };
+
+    VkViewport viewport{};
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+    viewport.width = (float)RenderPassResolution.x;
+    viewport.height = (float)RenderPassResolution.y;
+    viewport.minDepth = 0.0f;
+    viewport.maxDepth = 1.0f;
+
+    VkRect2D rect2D{};
+    rect2D.offset = { 0, 0 };
+    rect2D.extent = { (uint32_t)RenderPassResolution.x, (uint32_t)RenderPassResolution.y };
+
+    VkRenderPassBeginInfo renderPassInfo{};
+    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+    renderPassInfo.renderPass = renderPass;
+    renderPassInfo.framebuffer = RenderPassFramebuffer[VulkanRenderer::GetImageIndex()];
+    renderPassInfo.renderArea.offset = { 0, 0 };
+    renderPassInfo.renderArea.extent = { (uint32_t)RenderPassResolution.x, (uint32_t)RenderPassResolution.y };
+    renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+    renderPassInfo.pClearValues = clearValues.data();
+
+    VkCommandBuffer commandBuffer = CommandBuffer[VulkanRenderer::GetCMDIndex()];
+    if (vkBeginCommandBuffer(commandBuffer, &beginInfo) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to begin recording command buffer.");
+    }
+
+    vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+    vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+    vkCmdSetScissor(commandBuffer, 0, 1, &rect2D);
+    {
+        for (auto& mesh : MeshRendererManager::GetMeshList())
+        {
+            switch (mesh->GetMeshType())
+            {
+            case MeshTypeEnum::kPolygon:
+            {
+                depthCubeMapPipeline.Draw(commandBuffer, mesh, CubeSamplerPos);
+                break;
+            }
+            default:
+            {
+                break;
+            }
+            }
+        }
+    }
+    vkCmdEndRenderPass(commandBuffer);
+    if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to record command buffer.");
+    }
+
+    return commandBuffer;
+}
+
+void DepthCubeMapRenderer::Destroy()
+{
+    DepthTexture->Destroy();
+    depthCubeMapPipeline.Destroy();
+    RenderPass::Destroy();
+}
--- EclipseEngine/DepthCubeMapRenderer.h
+++ EclipseEngine/DepthCubeMapRenderer.h
@@ -1,5 +1,28 @@
 #pragma once
-class DepthCubeMapRenderer
-{
-};
+#include "RenderPass.h"
+#include "DepthCubeMapPipeline.h"
+#include "RenderedCubeMapDepthTexture.h"
 
+class DepthCubeMapRenderer : public RenderPass
+{
+private:
+	std::vector<VkPipelineColorBlendAttachmentState> ColorAttachmentList;
+	std::vector<VkPipelineShaderStageCreateInfo> PipelineShaderStageList;
+	VkVertexInputBindingDescription VertexInputBindingDescription;
+	std::vector<VkVertexInputAttributeDescription> VertexInputAttributeDescription;
+
+	DepthCubeMapPipeline depthCubeMapPipeline;
+
+	void RenderPassDesc();
+	void BuildRenderPassPipelines();
+
+public:
+	DepthCubeMapRenderer();
+	~DepthCubeMapRenderer();
+
+	std::shared_ptr<RenderedCubeMapDepthTexture> DepthTexture;
+
+	void BuildRenderPass(glm::vec2 TextureResolution);
+	VkCommandBuffer Draw(glm::vec3 CubeSamplerPos);
+	void Destroy();
+};
