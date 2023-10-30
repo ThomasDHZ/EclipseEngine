50f349b1cc07ef81b5124ed351f1e5a149622d0d
asdf <dothackzero@outlook.com>
2023-10-30 08:20:21 -0400

    temp

--- EclipseEngine/Scene.h
+++ EclipseEngine/Scene.h
@@ -1,41 +1,41 @@
-#pragma once
-#include "Window.h"
-#include "VulkanRenderer.h"
-#include "RenderPass2D.h"
-#include "InterfaceRenderPass.h"
-#include "FrameBufferRenderPass.h"
-#include "UniformBuffer.h"
-#include "Texture.h"
-#include "Vertex.h"
-#include "OrthographicCamera.h"
-#include "PerspectiveCamera.h"
-#include "GameObject2D.h"
-#include "GameObject3D.h"
-#include "RenderedTexture.h"
-#include "Texture2D.h"
-#include "SoundEffectManager.h"
-#include "SoundSource.h"
-#include "RayTracePBRRenderer.h"
-#include "ParticleSystem.h"
-#include "RenderPass.h"
-#include "GLTFRenderer2D.h"
-#include "PBRRenderer.h"
-
-class Scene
-{
-private:
-	PBRRenderer pbrRenderer;
-	//GLTFRenderer2D spriteRenderer;
-	//RayTracePBRRenderer rayTraceRenderer;
-public:
-	Scene();
-	~Scene();
-
-	void StartUp();
-	void Update();
-	void ImGuiUpdate();
-	void BuildRenderers();
-	void Draw();
-	void Destroy();
-};
-
+//#pragma once
+//#include "Window.h"
+//#include "VulkanRenderer.h"
+//#include "RenderPass2D.h"
+//#include "InterfaceRenderPass.h"
+//#include "FrameBufferRenderPass.h"
+//#include "UniformBuffer.h"
+//#include "Texture.h"
+//#include "Vertex.h"
+//#include "OrthographicCamera.h"
+//#include "PerspectiveCamera.h"
+//#include "GameObject2D.h"
+//#include "GameObject3D.h"
+//#include "RenderedTexture.h"
+//#include "Texture2D.h"
+//#include "SoundEffectManager.h"
+//#include "SoundSource.h"
+//#include "RayTracePBRRenderer.h"
+//#include "ParticleSystem.h"
+//#include "RenderPass.h"
+//#include "GLTFRenderer2D.h"
+//#include "PBRRenderer.h"
+//
+//class Scene
+//{
+//private:
+//	//PBRRenderer pbrRenderer;
+//	//GLTFRenderer2D spriteRenderer;
+//	RayTraceRenderer rayTraceRenderer;
+//public:
+//	Scene();
+//	~Scene();
+//
+//	void StartUp();
+//	void Update();
+//	void ImGuiUpdate();
+//	void BuildRenderers();
+//	void Draw();
+//	void Destroy();
+//};
+//
