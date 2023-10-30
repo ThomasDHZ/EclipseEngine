37d8da49c4f167e70660a14e0577edc86421338b
asdf <dothackzero@outlook.com>
2023-10-29 23:10:37 -0400

    Updated vulkan version.

--- EclipseEngine/GraphicsPipeline.cpp
+++ EclipseEngine/GraphicsPipeline.cpp
@@ -67,10 +67,11 @@ void GraphicsPipeline::BuildShaderPipeLine(BuildGraphicsPipelineInfo& buildPipel
     pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
     pipelineLayoutInfo.setLayoutCount = 1;
     pipelineLayoutInfo.pSetLayouts = &DescriptorSetLayout;
-    pipelineLayoutInfo.pushConstantRangeCount = 1;
 
     if (buildPipelineInfo.RenderPassDescription.ConstBufferSize != 0)
     {
+        pipelineLayoutInfo.pushConstantRangeCount = 1;
+
         VkPushConstantRange pushConstantRange{};
         pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
         pushConstantRange.offset = 0;
--- EclipseEngine/JsonGraphicsPipeline.cpp
+++ EclipseEngine/JsonGraphicsPipeline.cpp
@@ -144,9 +144,11 @@ void JsonGraphicsPipeline::LoadGraphicsPipeline(const std::string filePath, std:
         pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
         pipelineLayoutInfo.setLayoutCount = 1;
         pipelineLayoutInfo.pSetLayouts = &DescriptorSetLayout;
-        pipelineLayoutInfo.pushConstantRangeCount = 1;
+      
         if (sizeofConstBuffer != 0)
         {
+            pipelineLayoutInfo.pushConstantRangeCount = 1;
+
             VkPushConstantRange pushConstantRange{};
             pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
             pushConstantRange.offset = 0;
